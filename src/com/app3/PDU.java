5d46605662a8a381bb042e69f0e135b3f054e08c
Julien Rousseau <julrousseau20@gmail.com>
2021-10-18 15:09:26 -0400

    Couche liaison et PDU

--- src/com/app3/PDU.java
+++ src/com/app3/PDU.java
@@ -1,4 +1,35 @@
 package com.app3;
 
-public class PDU {
+import java.util.Arrays;
+
+public class PDU extends Prototype {
+    private byte[] content;
+
+    public byte[] getBytes() {
+        return content.clone();
+    }
+
+    public void ajouterEntete(byte[] entete) {
+        byte[] newContent = new byte[content.length + entete.length];
+        System.arraycopy(entete, 0, newContent, 0, entete.length);
+        System.arraycopy(content, 0, newContent, entete.length, content.length);
+        content = newContent;
+    }
+
+    public byte[] enleverEntete(int longueurEntete) throws Exception {
+        if (longueurEntete <= 0) {
+            // TODO
+            throw new Exception("Impossible de retirer une qte negative ou nulle. :)");
+        }
+
+        byte[] entete = Arrays.copyOfRange(content, 0, longueurEntete);
+        content = Arrays.copyOfRange(content, longueurEntete, content.length);
+
+        return entete;
+    }
+
+    @Override
+    public PDU clone() {
+        return (PDU) super.clone();
+    }
 }
--- src/com/app3/couche/CoucheLiaison.java
+++ src/com/app3/couche/CoucheLiaison.java
@@ -1,11 +1,24 @@
 package com.app3.couche;
 
+import com.app3.Log;
 import com.app3.PDU;
+import com.app3.crc.CRC;
+import com.app3.stats.StatRecord;
+import com.app3.stats.Statistiques;
 
-public class CoucheLiaison implements ICouche{
+import java.nio.ByteBuffer;
+
+public class CoucheLiaison implements ICouche {
+    ICouche nextCouche;
+
+    public CoucheLiaison() {
+
+    }
+
     @Override
     public void handle(PDU pdu) {
         //TODO
+
     }
 
     @Override
@@ -13,8 +26,42 @@ public class CoucheLiaison implements ICouche{
         //TODO
     }
 
+    private PDU ajouteCRC(PDU _pdu) {
+        PDU pdu = _pdu.clone();
+
+        int crc = CRC.generer(pdu.getBytes());
+        ByteBuffer buffer = ByteBuffer.allocate(4).putInt(crc);
+        pdu.ajouterEntete(buffer.array());
+
+        Log.enregistrer("Ajout du CRC au PDU.");
+        Statistiques.Instance().augmenterEnregistrement(StatRecord.QUANTITE_ENVOI);
+
+        return pdu;
+    }
+
+    private PDU retirerCRC(PDU _pdu) throws Exception {
+        PDU pdu = _pdu.clone();
+
+        Log.enregistrer("Retrait et validation du CRC sur le PDU.");
+        Statistiques.Instance().augmenterEnregistrement(StatRecord.QUANTITE_RECUE);
+
+        try {
+            byte[] crc = pdu.enleverEntete(32);
+            byte[] contenu = pdu.getBytes();
+
+            if (CRC.verifier(crc, contenu)) {
+                return pdu;
+            }
+            // TODO
+            throw new Exception("CRC invalide");
+        } catch (Exception ex) {
+            Statistiques.Instance().augmenterEnregistrement(StatRecord.QUANTITE_ERREUR);
+            throw ex;
+        }
+    }
+
     @Override
     public void close() {
-        //TODO
+        
     }
 }
