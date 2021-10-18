2bb8a63e4400eab46c93790af1b4df5665463c67
Julien Rousseau <julrousseau20@gmail.com>
2021-10-18 15:09:05 -0400

    Stats

---
+++ src/com/app3/stats/StatRecord.java
@@ -0,0 +1,8 @@
+package com.app3.stats;
+
+public enum StatRecord {
+    QUANTITE_ENVOI,
+    QUANTITE_RECUE,
+    QUANTITE_ERREUR,
+    QUANTITE_PERDUE
+}
---
+++ src/com/app3/stats/Statistiques.java
@@ -0,0 +1,24 @@
+package com.app3.stats;
+
+import java.util.*;
+
+public class Statistiques {
+    private static Statistiques instance = null;
+
+    private Map<StatRecord, Integer> stats;
+
+    private Statistiques() {
+        stats = new HashMap<StatRecord, Integer>();
+    }
+
+    public static Statistiques Instance() {
+        if (Statistiques.instance == null) {
+            Statistiques.instance = new Statistiques();
+        }
+        return Statistiques.instance;
+    }
+
+    public void augmenterEnregistrement(StatRecord enregistrement) {
+        stats.put(enregistrement, stats.get(enregistrement) + 1);
+    }
+}
