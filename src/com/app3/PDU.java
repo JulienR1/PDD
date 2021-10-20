87692ab1accbcfe782a04464c92ed132ea508a52
Samuel <lavs1609@usherbrooke.ca>
2021-10-19 20:58:27 -0400

    javadoc

--- src/com/app3/PDU.java
+++ src/com/app3/PDU.java
@@ -7,28 +7,55 @@ public class PDU implements Prototype {
     private byte[] content;
     private boolean estValide;
 
+    /**
+     * Constructeur normal de PDU.
+     * @param nom
+     * @param contenuInitial
+     */
     public PDU(String nom, byte[] contenuInitial) {
         this(nom, contenuInitial, true);
     }
 
+    /**
+     * Constructeur de PDU lors du televersement avec erreurs.
+     * @param nom
+     * @param contenuInitial
+     * @param estValide
+     */
     public PDU(String nom, byte[] contenuInitial, boolean estValide) {
         this.nom = nom;
         this.content = contenuInitial;
         this.estValide = estValide;
     }
 
+    /**
+     *
+     * @return Le contenu (donnees) du PDU dans un tableau de byte.
+     */
     public byte[] getBytes() {
         return content.clone();
     }
 
+    /**
+     *
+     * @return Le nom du fichier qui est televerser.
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     *
+     * @return La validite (avec erreur ou non) du PDU.
+     */
     public boolean getEstValide() {
         return estValide;
     }
 
+    /**
+     * Ajoute un entete aux PDU. En ajoutant l'entete a gauche du contenu dans un tableau de byte.
+     * @param entete
+     */
     public void ajouterEntete(byte[] entete) {
         byte[] newContent = new byte[content.length + entete.length];
         System.arraycopy(entete, 0, newContent, 0, entete.length);
@@ -36,6 +63,12 @@ public class PDU implements Prototype {
         content = newContent;
     }
 
+    /**
+     * Retire un nombre(longueurEntete) de bytes a gauche du tableau de byte du PDU.
+     * @param longueurEntete
+     * @return Le contenu sans l'entete a retirer (tableau de byte) du PDU.
+     * @throws Exception
+     */
     public byte[] enleverEntete(int longueurEntete) throws Exception {
         if (longueurEntete <= 0) {
             // TODO
@@ -48,6 +81,10 @@ public class PDU implements Prototype {
         return entete;
     }
 
+    /**
+     * Clone l'instance d'un PDU.
+     * @return Un nouveau PDU avec les memes valeurs.
+     */
     public PDU clone() {
         return new PDU(this.getNom(), getBytes(), estValide);
     }
--- src/com/app3/Serveur.java
+++ src/com/app3/Serveur.java
@@ -6,6 +6,11 @@ import com.app3.stats.Statistiques;
 import java.io.IOException;
 
 public class Serveur {
+    /**
+     * Initialise les couches du serveur et affiche les informations du serveur dans la console.
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         ICouche app = new CoucheApplication();
         ICouche transport = new CoucheTransport();
